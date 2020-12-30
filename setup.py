a16348d75a74905f34a5d6664a227919e56ea339
Will Chen <wchen108@uw.edu>
2020-12-30 00:33:00 -0800

    update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="primedel",
-    version="1.0",
+    version="1.1",
     author="Wei Chen",
     author_email="wchen108@uw.edu",
     description="A package for primedel",
